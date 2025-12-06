f821062d0c9d46693c36d80316acd5c421aa2152
Your Name <you@example.com>
2025-12-06 14:16:59 +0200

    commit

--- nextjs-dashboard/next.config.ts
+++ nextjs-dashboard/next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    turbo: {
+      rules: {
+        "*.jsx": ["jsx"],
+        "*.tsx": ["tsx"]
+      }
+    }
+  }
 };
 
-export default nextConfig;
+module.exports = nextConfig;
